e50e507a49a9e83fbc964bd7834928b3f81d4321
DanLDB <dpbergquist>
2015-05-20 17:46:29 +0100

    add marker - seems to auto-update

--- public/javascripts/mapController.js
+++ public/javascripts/mapController.js
@@ -1,10 +1,31 @@
-mapular.controller('MapCtrl', ['uiGmapGoogleMapApi', '$geolocation', function(uiGmapGoogleMapApi, $geolocation){
-  var vm = this;
-  
+mapular.controller('MapCtrl', ['$scope', 'uiGmapGoogleMapApi', '$geolocation', function($scope, uiGmapGoogleMapApi, $geolocation){
+  var viewModel = this;
+  var updateMap = function() {
+    viewModel.map = {
+      center: {
+        latitude: viewModel.myPosition.coords.latitude,
+        longitude: viewModel.myPosition.coords.longitude
+      },
+      zoom: 16
+    };
+  };
+
   $geolocation.getCurrentPosition({
-      timeout: 60000
-   }).then(function(position) {
-      vm.myPosition = position;
-      vm.map = { center: { latitude: vm.myPosition.coords.latitude, longitude: vm.myPosition.coords.longitude }, zoom: 8 };
-   });
+    timeout: 60000
+  }).then(function(position) {
+    viewModel.myPosition = position;
+    updateMap();
+    viewModel.myPosition = $geolocation.position;
+  });
+  
+  $geolocation.watchPosition({
+    timeout: 60000,
+    maximumAge: 250,
+    enableHighAccuracy: true
+  });
+
+
+
+
+
 }])
