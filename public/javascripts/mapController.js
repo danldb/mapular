7bc7eba4955afd8f5d011a0655a12c0a652cdcc7
DanLDB <dpbergquist>
2015-05-21 11:03:26 +0100

    map now recenters as you move

--- public/javascripts/mapController.js
+++ public/javascripts/mapController.js
@@ -1,13 +1,6 @@
 mapular.controller('MapCtrl', ['uiGmapGoogleMapApi', '$geolocation', function(uiGmapGoogleMapApi, $geolocation){
   var viewModel = this;
 
-  viewModel.myPosition = {
-    coords: {
-      latitude: 51.5173913,
-      longitude: -0.0733216
-    }
-  }
-
   $geolocation.watchPosition({
     timeout: 60000,
     maximumAge: 250,
@@ -15,13 +8,9 @@ mapular.controller('MapCtrl', ['uiGmapGoogleMapApi', '$geolocation', function(ui
   });
 
   viewModel.map = {
-    center: {
-      latitude: viewModel.myPosition.coords.latitude,
-      longitude: viewModel.myPosition.coords.longitude
-    },
     zoom: 16
   };
   
   viewModel.myPosition = $geolocation.position;
-  
+
 }])
